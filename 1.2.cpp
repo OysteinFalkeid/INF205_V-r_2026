5231ebe7263251087147ee014348ff7aad11587c
Venuga Sivarajah <116897071+nuggizz@users.noreply.github.com>
2026-02-20 12:04:59 +0100

    Update 1.2.cpp

--- 1.2.cpp
+++ 1.2.cpp
@@ -1,6 +1,53 @@
 #include <iostream>
 using namespace std;
 
+void print_s(short value){
+    unsigned char* p = (unsigned char*)&value;
+
+    std::cout << "short " << value << " , size = " << sizeof(value) << " bytes" << "\n";
+    //Gå gjennom minnet en byte av gangen
+    //Skrive ut hver byte i hex
+    for (int j = 0; j < sizeof(value); j++){
+        std::cout << "0x" << hex << (int)p[j] << " ";
+    }
+    cout << dec << "\n\n";
+}
+
+void print_i(int value){
+    unsigned char* p = (unsigned char*)&value;
+
+    std::cout << "int " << value << " , size = " << sizeof(value) << " bytes" << "\n";
+    //Gå gjennom minnet en byte av gangen
+    //Skrive ut hver byte i hex
+    for (int j = 0; j < sizeof(value); j++){
+        std::cout << "0x" << hex << (int)p[j] << " ";
+    }
+    cout << dec << "\n\n";
+}
+
+void print_l(long value){
+    unsigned char* p = (unsigned char*)&value;
+
+    std::cout << "long " << value << " , size = " << sizeof(value) << " bytes" << "\n";
+    //Gå gjennom minnet en byte av gangen
+    //Skrive ut hver byte i hex
+    for (int j = 0; j < sizeof(value); j++){
+        std::cout << "0x" << hex << (int)p[j] << " ";
+    }
+    cout << dec << "\n\n";
+}
+
+void print_ll(long long value){
+    unsigned char* p = (unsigned char*)&value;
+
+    std::cout << "long long " << value << " , size = " << sizeof(value) << " bytes" << "\n";
+    //Gå gjennom minnet en byte av gangen
+    //Skrive ut hver byte i hex
+    for (int j = 0; j < sizeof(value); j++){
+        std::cout << "0x" << hex << (int)p[j] << " ";
+    }
+    cout << hex << "\n\n";
+}
 int main(){
     // Lagrer tallverdier - 12 i ulike datatyper:
 
@@ -12,13 +59,9 @@ int main(){
     // Ta adressen til en datatype
     //Tolk den som en char*. Bruker unsigned char for å unngå negative verdier
 
-    unsigned char* p = (unsigned char*)&i;
-
-    //Gå gjennom minnet en byte av gangen
-    //Skrive ut hver byte i hex
-
-    for (int j = 0; j < sizeof(i); j++){
-        cout << "0x" << hex << (int)p[i] << " ";
-    }
+    print_s(s);
+    print_i(i);
+    print_l(l);
+    print_ll(ll);
 
 }
