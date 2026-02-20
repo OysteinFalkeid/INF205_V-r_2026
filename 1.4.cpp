384447d58ed8b56bc29a048ba152d6d0b39e1cf0
Venuga Sivarajah <116897071+nuggizz@users.noreply.github.com>
2026-02-20 12:07:04 +0100

    Update 1.4.cpp

--- 1.4.cpp
+++ 1.4.cpp
@@ -16,31 +16,35 @@ namespace
 
 int main()
 {
+   //Åpner fila for lesing
    std::ifstream infile("Bookindex.txt");
 
+   //Sjekker om fila eksisterer, gir feilmelding ellers
    if (!infile){
     std::cout << "Kan ikke åpne filen for lesing" << "\n";
     return 1;
    }
 
+   // leser inn første taller, n er første tallet i fila
    int n;
    infile >> n;
 
+   //Oppretter en tom vektor som kan lagre objektene
    std::vector<litindex::BookIndex> books;
 
    for (int i = 0; i < n; i++){
-    int c,s,p;
-    infile >> c >> s >> p;
-    litindex::BookIndex b(c,s,p);
-    books.push_back(b);
+      //Leser en linje fra fila
+      int c,s,p;
+      infile >> c >> s >> p;
+      //Lager objektet og kaller på konstruktøren
+      litindex::BookIndex b(c,s,p);
+      books.push_back(b); //Legger til bakerst i vektoren. 
+      //Så den første som blir bakerst havner først når løkka er ferdig
     }
 
-   std::cout << "\nBookIndex example:\n";
-   litindex::BookIndex idx(1, 11, 24);
-   
-   
-   
-   idx.out();  // print status
-   start_chapter(idx);
-   idx.out();  // print status
+   std::cout << "\nObjekter lest fra fil:\n";
+   // Går gjennom hvert element i vektoren og skriver ut. Kaller på out funksjonen i h-fila
+   for (const auto& b : books) {
+    b.out();
+   }
 }
