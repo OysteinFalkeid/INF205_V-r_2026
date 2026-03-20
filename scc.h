82ec98b6ab17f14ea5e765bc23ff114df146b817
Venuga Sivarajah <116897071+nuggizz@users.noreply.github.com>
2026-03-20 10:33:08 +0100

    Lagt til 3-2

--- scc.h
+++ scc.h
@@ -11,4 +11,8 @@
 
 std::vector<std::vector<std::string>> tarjan_scc(AbstractGraph* graph);
 
-
+std::vector<std::pair<std::string,std::string>>
+find_diamonds(
+    AbstractGraph* g,
+    const std::set<std::string>& even_labels,
+    const std::set<std::string>& odd_labels);
