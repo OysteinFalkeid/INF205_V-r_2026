dab2c09eaa0c4f84143781a45f96fd548cc79859
Venuga Sivarajah <116897071+nuggizz@users.noreply.github.com>
2026-03-30 20:44:02 +0200

    Update scc.h

--- scc.h
+++ scc.h
@@ -1,6 +1,5 @@
 #pragma once
 #include "graph.h"
-
 #include <set>
 #include <vector>
 #include <string>
@@ -14,5 +13,5 @@ std::vector<std::vector<std::string>> tarjan_scc(AbstractGraph* graph);
 std::vector<std::pair<std::string,std::string>>
 find_diamonds(
     AbstractGraph* g,
-    const std::set<std::string>& even_labels,
-    const std::set<std::string>& odd_labels);
+    const std::vector<std::string>& even_labels,
+    const std::vector<std::string>& odd_labels);
